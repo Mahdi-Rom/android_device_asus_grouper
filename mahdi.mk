70c2216e5741d13ecc73d5c0bb42b0b8b0764907
Altaf-Mahdi <altaf.mahdi@gmail.com>
2013-08-04 01:50:44 +0100

    add common.mk

--- mahdi.mk
+++ mahdi.mk
@@ -5,7 +5,10 @@ PRODUCT_RELEASE_NAME := Nexus7
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 800
 
-# Inherit some common stuff.
+# Inherit common product files.
+$(call inherit-product, vendor/mahdi/config/common.mk)
+
+# Inherit tablet config.
 $(call inherit-product, vendor/mahdi/config/common_full_tablet_wifionly.mk)
 
 # Enhanced NFC
